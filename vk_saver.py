a003811b5130108cbbbc371558d0cfd6206ba1cf
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2014-05-16 01:36:32 +0400

    initial version

---
+++ vk_saver.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# usage: save an html file containing mp3 links from vk.com 
+# ("view source" in your web browser (ctrl+u in Chrome))
+# vk_saver < your.html
+# enjoy
+
+from re import *
+import re
+import sys
+from subprocess import call
+regex = re.compile("3pm\.[^\"]+?/:sptth")
+
+a = []
+for line in sys.stdin:
+	line = ''.join(reversed(line))
+	song_tuple = map (lambda s: "".join(reversed(s)), regex.findall(line))
+	for song in song_tuple: 
+		call(["wget -c", song])
