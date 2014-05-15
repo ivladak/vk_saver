02677cf2643b53c23cd992a711a01e0bf1f50590
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2014-05-16 02:20:49 +0400

    More natural way of passing input file. Do not re-download

--- vk_saver.py
+++ vk_saver.py
@@ -2,18 +2,22 @@
 
 # usage: save an html file containing mp3 links from vk.com 
 # ("view source" in your web browser (ctrl+u in Chrome))
-# ./vk_saver < your.html
+# ./vk_saver your.html
 # enjoy
 
 from re import *
 import re
 import sys
-from subprocess import call
+from subprocess import * 
 regex = re.compile("3pm\.[^\"]+?/:sptth")
 
-for line in sys.stdin:
+if len(sys.argv) < 2:
+	print "please specify source html file"
+	exit()	
+
+for line in open(sys.argv[1]):
 	line = line[::-1]
 	song_tuple = map (lambda s: s[::-1], regex.findall(line))
 	for song in song_tuple: 
-		call(["wget", song])
-#		call(["wget --no-clobber", song])
+		p = Popen(["wget", "--no-clobber", song], stdout=PIPE)
+		print(p.communicate()[0])
