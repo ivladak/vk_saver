938bc730f0f1833b5acdc0b8c51486311e21066f
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2014-12-22 21:36:26 +0300

    include the 0-th song

--- vkSaver.js
+++ vkSaver.js
@@ -3,7 +3,7 @@
 
 var patt = /https:.*mp3/
 
-for (var i = 1; i < document.getElementsByClassName("title_wrap fl_l").length; i++) {
+for (var i = 0; i < document.getElementsByClassName("title_wrap fl_l").length; i++) {
 	linkk = patt.exec(document.getElementsByClassName("play_btn fl_l")[i].getElementsByTagName("input")[0].value)[0]
 	namee = document.getElementsByClassName("title_wrap fl_l")[i].outerText
 	console.log(linkk, namee)
