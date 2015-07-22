d45fdde77b06393486e2cbfcebf8e45ddbb0eaee
Vladislav Ivanishin <vlad@ispras.ru>
2015-07-22 13:32:01 +0300

    Adapt to vk changes: https -> http

--- vkSaver.js
+++ vkSaver.js
@@ -1,7 +1,7 @@
 //document.getElementsByClassName("title_wrap fl_l")[43].outerText
 //document.getElementsByClassName("play_btn fl_l")[43].getElementsByTagName("input")[0].value
 
-var patt = /https:.*mp3/
+var patt = /http:.*mp3/
 
 for (var i = 0; i < document.getElementsByClassName("title_wrap fl_l").length; i++) {
 	linkk = patt.exec(document.getElementsByClassName("play_btn fl_l")[i].getElementsByTagName("input")[0].value)[0]
